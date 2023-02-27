7374d580b75a175e1bd363be9fecad0bb2228ad3
Alexander van der Leek <alexandervanderleek@gmail.com>
2023-02-27 08:53:55 +0200

    27feb

--- client.py
+++ client.py
@@ -1,12 +1,16 @@
 import socket
 import os
- 
+import math
+import fcntl
+import struct
 
 
 #192.168.1.118
 SIZE = 1024
- 
+
+
 def main():
+
     while True:
         IP = input("Enter IP Address for the server:\n")
         PORT = input("Enter PORT on server:\n")
@@ -31,11 +35,13 @@ def main():
             while True:
                 match command:
                     case "help":
+                        os.system('cls')
                         print("view -> see all files on our server")
                         print("upload -> upload a new file to our server")
                         print("download -> download a file from our server")
                         print("quit -> exit the application" )
                         print("help -> get list of commands" )
+                        print("clear -> clear the screen" )
                         command = input("")
                     case "upload":
                         while True:
@@ -92,8 +98,8 @@ def main():
                                             break
                                 
                                 
-                                
                                 toSend = "upload,"+visbilityFinal+","+downloadabilityFinal+","+key+","+ str(socket.gethostbyname(socket.gethostname())) +","+ str(os.path.getsize(file))+","+os.path.basename(file)
+                                print("Upload in progress...")
                                 client.send(toSend.encode())
 
                              
@@ -103,6 +109,14 @@ def main():
                                     client.send(l)
                                     l = f.read(1024)
                                 f.close()
+
+                                response = client.recv(SIZE).decode()
+
+                                if(response == "OK"):
+                                    os.system('cls')
+                                    print("succesfully received file")
+                                else:
+                                    print(response)
 
                                 command = input("Type help for a list of commands.\n")
                                 break
@@ -128,13 +142,128 @@ def main():
                         if(next == "y" or next =="yes"):
                             key = input("Enter your secret key\n")
 
-
+                        os.system('cls')
                         client.send(("view,"+key+","+str(socket.gethostbyname(socket.gethostname()))).encode())
+                        
+
+                        result = client.recv(SIZE).decode()
+
+                        if(result!="ERROR"):
+                        
+                            titles = ["FileName:","Downloadable:","Restriction:"]
+                            print("{: >15} {: >15} {: >15}".format(*titles))
+                            for item in result.split(","):
+                                newItem = item.split("/")
+                                print("{: >15} {: >15} {: >15}".format(*newItem))                        
+
+                            
+                            print("\n")
+                        else:
+                            print("SOMETHING HAS GONE WRONG")
+
                         command = input("Type help for a list of commands.\n")
+                    
+                    case "download":
+                        while True:
+                            filename = input("Enter the name or your file to download.\n")
+                            key=""
+                            visbility = input("Is your file a public file. (y/n):\n")
+                            while True:
+                                if(visbility != "y" and visbility != "n" and visbility != "yes" and visbility != "no"):
+                                    visbility = input("Invalid response. Try again (y/n)\n")
+                                else:
+                                    if(visbility == "y" or visbility=="yes"):
+                                        visbilityFinal = "open"
+                                    else:
+                                        visbilityFinal = "closed"
+                                    break
+                            downloadability = input("Is this file downloadable for all. (y/n).\n")
+                            while True:
+                                if(downloadability != "y" and downloadability != "n" and downloadability != "yes" and downloadability != "no"):
+                                    downloadability = input("Invalid response. Try again (y/n).\n")
+                                else:
+                                    if(downloadability == "y" or downloadability=="yes"):
+                                        downloadabilityFinal = "open"
+                                        break
+                                    else:
+                                        downloadability = input("Specify restriction type. (clientOnly/key):\n")
+                                        while True:
+                                            match downloadability:
+                                                case 'key':
+                                                    invalid = ["/","<",">",":",'"','\\', "|","?","*"]
+                                                    downloadabilityFinal = "key"
+                                                    key = input("Enter your secret key.\n")
+                                                    while True:
+                                                        found = False
+                                                        for item in invalid:
+                                                            if(key.find(item)>-1):
+                                                                found = True
+                                                                key = input("Invalid character in secret key. Please re-enter.\n")
+                                                                break
+
+                                                        if(found==False):
+
+                                                            break
+                                                    
+                                                    break
+                                                case 'clientOnly':
+                                                    downloadabilityFinal = "client"
+                                                    break
+                                                case _:
+                                                    downloadability = input("Invalid response. Try again (clientOnly/key).\n")
+                                        break
+                            
+                            
+                            toSend = "download,"+visbilityFinal+","+downloadabilityFinal+","+key+","+ str(socket.gethostbyname(socket.gethostname()))+","+filename
+                            print("Download in progress...")
+                            client.send(toSend.encode())
+
+
+                            response = client.recv(SIZE).decode()
+                            if("ERROR," in response):
+                                os.system('cls')
+                                print(response)
+                            else:
+                                os.makedirs(os.path.dirname("./downloads/"+filename), exist_ok=True)
+                                with open("./downloads/"+filename, "wb") as f:
+                                    for x in range(math.ceil(int(response)/SIZE)):
+                                        bytes_read =client.recv(SIZE)
+                                        f.write(bytes_read)
+                                    f.close()
+
+                            
+                                if(os.path.getsize("./downloads/"+filename) == int(response)):
+                                    os.system('cls')
+                                    print("succesfully received file")
+                                else:
+                                    print("ERROR DID NOT RECEIVE ENTIRE FILE")
+                                    os.remove("./download/"+filename)
+
+
+                            command = input("Type help for a list of commands.\n")
+                            break
+                        
+                
+
+
+
+
+
+
 
                     case "quit":
+                        os.system('cls')
+                        client.send("quit,".encode())
                         client.close()
+                        print("Graceful exit.")
                         exit()
+
+                    case "clear":
+                        os.system('cls')
+                        command = input("Type help for a list of commands.\n")
+
+
+
                     case _:
                         print("Could not find that command.")
                         command = input("Type help for a list of commands.\n")
--- server/server.py
+++ server/server.py
@@ -2,6 +2,8 @@ import socket
 import os
 import math
  
+#SETTING BUFFER SIZE AND IP AND PORT FOR SERVER
+
 IP = socket.gethostbyname(socket.gethostname())
 PORT = 4455
 ADDR = (IP, PORT)
@@ -11,66 +13,178 @@ SIZE = 1024
 def main():
     print("[STARTING] Server is starting.")
     print(IP)
-    """ Staring a TCP socket. """
+
+    #Create our server bind the ADDR and set a waiting q of 5
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-
-    """ Bind the IP and PORT to the server. """
     server.bind(ADDR)
- 
-    """ Server is listening, i.e., server is now waiting for the client to connected. """
-    server.listen()
-    print("[LISTENING] Server is listening.")
- 
-    while True:
-        """ Server has accepted the connection from the client. """
-        conn, addr = server.accept()
-        print(f"[NEW CONNECTION] {addr} connected.")
+    server.listen(5)
 
+
+    #Our main loop to keep server running and checking for connections
+    while True:
+
+        #Accept a connection and send a OK response to the client
+        conn, addr = server.accept()
         conn.send('OK'.encode())
         
-
+        #our inner while loop for taking client commands
         while True:
+
+            #accept client commands, create a array of 'header' style data
             received = conn.recv(SIZE).decode()
             data_split = received.split(",")
+            
+            #match first client header data, dictating the clients command
             match data_split[0]:
+
+                #UPLOAD COMMAND CASE
+                #This case allows clients to upload files with visibility options of open or closed and
+                #downloadability options of client specific restriction and secret key based
+                #confirmation done through checking of file byte size
+
                 case "upload":
-                    url = ""
-                    match data_split[1]:
-                        case "open":
-                            url += "./open/"
-                        case "closed":
-                            url += "./closed/"
-                    
-                    match data_split[2]:
-                        case "client":
-                            url +=  data_split[4] + "/"
-                        case "key":
-                            url +=  data_split[3] + "/"
+                    try:
+                        #Build out a file pathname based on file privacy options (found in header array)
+                        url = ""
+                        match data_split[1]:
+                            case "open":
+                                url += "./open/"
+                            case "closed":
+                                url += "./closed/"
+                        
+                        match data_split[2]:
+                            case "client":
+                                url +=  data_split[4] + "/"
+                            case "key":
+                                url +=  data_split[3] + "/"
 
-
-                    url = url + data_split[6]
-                    os.makedirs(os.path.dirname(url), exist_ok=True)
-                    with open(url, "wb") as f:
-                        for x in range(math.ceil(int(data_split[5])/SIZE)):
-                            bytes_read =conn.recv(SIZE)
-                            f.write(bytes_read)
-                        f.close()
-                case "view":
-                    for subdir, dirs, files in os.walk('./open/'):
-                        for file in files:
-                            if(len(subdir)>7):
-                  
-                                if(data_split[2] in subdir):
-                                    print(file + " [d]")
-                                elif(data_split[1] in subdir and data_split[1] != ""):
-                                    print(file + " [d]")
-                                else:
-                                    if(data_split[1]==""):
-                                        print(file + "[x]" +"clientOnly restriction")
-                                    else:
-                                        print(file + "[x]" +"key restriction")
+                        #our final file pathname (where file to be stored)
+                        url = url + data_split[6]
+                        
+                        #Checking for if that file already exists and sends error with error code
+                        if(os.path.isfile(url)):
+                            conn.send('ERROR,FILE WITH THAT NAME EXITS'.encode())
+                        #otherwise build the file at the pathname (taking in binary of file)
+                        else:
+                            os.makedirs(os.path.dirname(url), exist_ok=True)
+                            with open(url, "wb") as f:
+                                for x in range(math.ceil(int(data_split[5])/SIZE)):
+                                    bytes_read =conn.recv(SIZE)
+                                    f.write(bytes_read)
+                                f.close()
+                            
+                            #check that size of file built is same as size of file on client side, data from header
+                            if data_split[5] == str(os.path.getsize(url)):
+                                conn.send('OK'.encode())
                             else:
-                                print(file + "[d]")
+                                os.remove(url)
+                                conn.send('ERROR,NOT ALL OF FILE RECEIVED'.encode())
+                    except:
+                        conn.send('ERROR,SOMETHING HAS GONE WRONG'.encode())
+
+
+                #VIEW COMMAND CASE
+                #This case allows clients to see all of there files they have access to "non-visible" and "visible"
+                #including files visible but not downloadable
+                case "view":
+
+                    try:
+                        downloadable=""
+
+                        #Go through all files in open folder (files visibles)
+                        for subdir, dirs, files in os.walk('./open/'):
+                            for file in files:
+
+                                #if it is a download protected file
+                                if(len(subdir)>7):
+
+                                    #check if you have correct ip/key to download file
+                                    if(data_split[2] in subdir):
+                                        downloadable+=(file+ "/y/client,")
+                                    elif(data_split[1] in subdir and data_split[1] != ""):
+                                        downloadable+=(file+ "/y/key,")
+                                    else:
+                                        if(subdir.count(".")==4):
+                                            downloadable+=(file + "/n/client,")
+                                        else:
+                                            downloadable+=(file + "/n/key,")
+                                else:
+                                    downloadable+=(file + "/y/,")
+
+                        #Go through all files in closed folder that might be yours (files not visible to all)
+                        for subdir, dirs, files in os.walk('./closed/'):
+                            for file in files:
+                                if(len(subdir)>7):
+                                    #check if you have correct ip/key to download file
+                                    if(data_split[2] in subdir):
+                                        downloadable+=(file+ "/y/client,")
+                                    elif(data_split[1] in subdir and data_split[1] != ""):
+                                        downloadable+=(file+ "/y/key,")
+                
+                                
+
+
+                        #send back all files and download status and restriction types
+                        conn.send(downloadable[:-1].encode())
+
+                    except:
+                        conn.send("ERROR".encode())
+                
+
+                
+                #Download command case
+                #enter specifications for file requested and download it
+                case "download":
+                    try:
+                        #Build the pathname for where file is to be downloaded
+                        url = ""
+                        match data_split[1]:
+                            case "open":
+                                url += "./open/"
+                            case "closed":
+                                url += "./closed/"
+                        
+                        match data_split[2]:
+                            case "client":
+                                url +=  data_split[4] + "/"
+                            case "key":
+                                url +=  data_split[3] + "/"
+
+                        #our final file pathname (where file should be)
+                        url = url + data_split[5]
+
+
+                        #Checking for if that file exists and sends error with error code if not
+                        if(os.path.isfile(url)):
+                             conn.send(str(os.path.getsize(url)).encode())
+
+                             f = open(url,'rb')
+                             l = f.read(1024)
+                             while (l):
+                                conn.send(l)
+                                l = f.read(1024)
+                             f.close()
+
+                        #otherwise error no file found
+                        else:
+                            restriction = ""
+                            if(data_split[2]=="client"):
+                                restriction = "with "+data_split[2] + " of "+data_split[4]
+                            elif(data_split[2]=="key"):
+                                restriction = "with "+data_split[2] + " of "+data_split[3]
+                            conn.send(('ERROR,File not in '+data_split[1]+ ' visbility folder '+restriction).encode())
+                    except:
+                        conn.send('ERROR,SOMETHING HAS GONE WRONG'.encode())
+
+
+
+                #QUIT COMMAND CASE
+                case "quit":
+                    conn.close()
+                    break
+
+
+
+
 if __name__ == "__main__":
     main()
